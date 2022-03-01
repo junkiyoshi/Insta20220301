d476924b568af9363261361e9b8e9194bd9a55cc
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2022-03-01 18:36:33 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,77 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(30);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(255);
+	ofSetLineWidth(2);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+
+	if (ofGetFrameNum() % 60 < 35) {
+
+		this->noise_param += ofMap(ofGetFrameNum() % 60, 0, 35, 0.14, 0);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofTranslate(ofGetWindowSize() * 0.5);
+
+	float radius = 200;
+	for (int deg = 0; deg < 360; deg += 5) {
+
+		auto base_location = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
+		auto noise_value = ofNoise(glm::vec3(base_location * 0.010, this->noise_param));
+		auto noise_radius = radius * (noise_value + 0.5);
+
+		this->draw_arrow(base_location, glm::vec2(noise_radius * cos(deg * DEG_TO_RAD), noise_radius * sin(deg * DEG_TO_RAD)), 18, ofColor(0));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw_arrow(glm::vec2 location, glm::vec2 target, float size, ofColor color) {
+
+	auto angle = std::atan2(target.y - location.y, target.x - location.x);
+	auto distance = glm::distance(target, location);
+
+	if (glm::length(distance) > size * 0.1) {
+
+		ofPushMatrix();
+		ofTranslate(target);
+
+		ofSetColor(color);
+		ofFill();
+		ofBeginShape();
+		ofVertex(glm::vec2(size * cos(angle), size * sin(angle)));
+		ofVertex(glm::vec2(size * 0.25 * cos(angle + PI * 0.5), size * 0.25 * sin(angle + PI * 0.5)));
+		ofVertex(glm::vec2(size * 0.25 * cos(angle - PI * 0.5), size * 0.25 * sin(angle - PI * 0.5)));
+		ofEndShape();
+
+		ofBeginShape();
+		ofVertex(glm::vec2(size * 0.25 * cos(angle + PI * 0.5), size * 0.25 * sin(angle + PI * 0.5)) * 0.25);
+		ofVertex(glm::vec2(size * 0.25 * cos(angle + PI * 0.5), size * 0.25 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(distance * cos(angle), distance * sin(angle)));
+		ofVertex(glm::vec2(size * 0.25 * cos(angle - PI * 0.5), size * 0.25 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(distance * cos(angle), distance * sin(angle)));
+		ofVertex(glm::vec2(size * 0.25 * cos(angle - PI * 0.5), size * 0.25 * sin(angle - PI * 0.5)) * 0.25);
+		ofEndShape();
+
+		ofPopMatrix();
+	}
+
+	ofDrawCircle(location, 5);
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
